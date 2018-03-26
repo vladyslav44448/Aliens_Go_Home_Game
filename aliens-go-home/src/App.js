c4ed91daf9587d6b07bd36325a9a8abf28733aa2
vladyslav_app <vladyslav.mykhalchenko@mail.ru>
2018-03-26 13:42:19 +0200

    Prepared to start creating game

---
+++ aliens-go-home/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h1>We will create an awesome game with React, Redux, and SVG!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
